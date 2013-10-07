95ea821baab29b68322f84e4b4590e7b20b0650f
PowerMogli <albix@gmx.net>
2013-10-07 23:49:36 +0200

    Methoden zum Parent hin verschoben.

--- src/Micro+/Query/ProcedureObject.cs
+++ src/Micro+/Query/ProcedureObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 
@@ -18,8 +19,27 @@ namespace MicroORM.Query
 
         protected Dictionary<string, IDataParameter> Parameters { get { return _parameters; } }
 
-        protected abstract bool AddParameter<T>(string parameterName, T value, DbType dbType, int length);
-        protected abstract T GetParameterValue<T>(string parameterName);
+        protected virtual bool AddParameter<T>(string parameterName, T value, DbType dbType, int length)
+        {
+            if (value is string && string.IsNullOrWhiteSpace(value.ToString())) throw new ArgumentNullException("value");
+
+            string stringValue = value as string;
+            if (!string.IsNullOrWhiteSpace(stringValue)
+                && length > 0
+                && stringValue.Length > length) return false;
+
+            return true;
+        }
+
+        protected T GetParameterValue<T>(string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(parameterName)) throw new ArgumentNullException("parameterName");
+
+            var parameter = this.Parameters[parameterName];
+            if (parameter == null) return default(T);
+
+            return (T)parameter.Value;
+        }
 
         public IEnumerator GetEnumerator()
         {
--- src/Micro+/Query/SqlProcedureObject.cs
+++ src/Micro+/Query/SqlProcedureObject.cs
@@ -11,12 +11,7 @@ namespace MicroORM.Query
 
         protected override bool AddParameter<T>(string parameterName, T value, DbType dbType, int length = -1)
         {
-            if (value is string && string.IsNullOrWhiteSpace(value.ToString())) throw new ArgumentNullException("value");
-
-            string stringValue = value as string;
-            if (!string.IsNullOrWhiteSpace(stringValue)
-                && length > 0
-                && stringValue.Length > length) return false;
+            if (base.AddParameter(parameterName, value, dbType, length) == false) return false;
 
             SqlParameter parameter = new SqlParameter(parameterName, value) { DbType = dbType };
             if (length > 0)
@@ -24,16 +19,6 @@ namespace MicroORM.Query
 
             base.Parameters.Add(parameterName, parameter);
             return true;
-        }
-
-        protected override T GetParameterValue<T>(string parameterName)
-        {
-            if (string.IsNullOrWhiteSpace(parameterName)) throw new ArgumentNullException("parameterName");
-
-            var parameter = base.Parameters[parameterName];
-            if (parameter == null) return default(T);
-
-            return (T)parameter.Value;
         }
     }
 }
