21f5471f091633e52530a7364a44983c01b7dd9a
PowerMogli <albix@gmx.net>
2013-10-10 22:12:47 +0200

    InvalidTypeException hinzugefügt.

---
+++ src/Micro+/InvalidTypeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MicroORM
+{
+    class InvalidTypeException : Exception
+    {
+        internal InvalidTypeException()
+            : base() { }
+
+        internal InvalidTypeException(string message)
+            : base(message) { }
+
+        internal InvalidTypeException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
