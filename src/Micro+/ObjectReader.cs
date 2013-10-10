43eabd35154798ca2d37d952c32d807b09b4cf9a
PowerMogli <albix@gmx.net>
2013-10-10 22:17:25 +0200

    Liest durch den DataReader und startet den Vorgang der Materialisierung.

--- src/Micro+/ObjectReader.cs
+++ src/Micro+/ObjectReader.cs
@@ -1,30 +1,55 @@
 ﻿using System;
 using System.Data;
 using MicroORM.Materialization;
+using MicroORM.Storage;
+using System.Collections.Generic;
 
 namespace MicroORM.Base
 {
     internal class ObjectReader<T> : IDisposable
     {
         private IDataReader _dataReader;
-        private Materializer _materlizer;
-        private DataSchema _dataSchema;
+        private EntityMaterializer _materlizer;
+        private DataReaderSchema _dataReaderSchema;
+        private IDbProvider _dbProvider;
 
-        internal ObjectReader(IDataReader dataReader)
+        internal ObjectReader(IDataReader dataReader, IDbProvider dbProvider)
         {
+            _dbProvider = dbProvider;
             _dataReader = dataReader;
+            _materlizer = new EntityMaterializer(dbProvider);
+            _dataReaderSchema = new DataReaderSchema(dataReader, typeof(T));
         }
 
-        public T Current { get; private set; }
+        internal T Current { get; private set; }
 
-        public bool Read()
+        internal bool Read()
         {
-            return false;
+            return Read(1);
+        }
+
+        internal bool Read(int step)
+        {
+            if (step < 0)
+                throw new ArgumentException("Step is lower then 1. Not allowed.", "step");
+
+            for (int i = 0; i < step; i++)
+            {
+                if (_dataReader.Read() == false) return false;
+            }
+
+            return ReadInternal();
+        }
+
+        private bool ReadInternal()
+        {
+            this.Current = _materlizer.Materialize<T>(_dataReaderSchema, _dataReader);
+            return true;
         }
 
         public void Dispose()
         {
-            if (_dataReader == null) return;
+            if (_dataReader == null || _dataReader.IsClosed) return;
 
             _dataReader.Close();
             _dataReader.Dispose();
