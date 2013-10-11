162450e6e7ba28c3fe747516c7606679f17cc887
PowerMogli <PowerMogli@users.noreply.github.com>
2013-10-11 17:12:03 +0200

    Value container with information about value state.

---
+++ src/Micro+/EntityValue.cs
@@ -0,0 +1,19 @@
+namespace MicroORM.Base
+{
+    public struct EntityValue<TValue>
+    {
+        public readonly bool IsDirty;
+        public readonly TValue Value;
+
+        public EntityValue(TValue value)
+        {
+            this.IsDirty = true;
+            this.Value = value;
+        }
+
+        public static implicit operator EntityValue<TValue>(TValue value)
+        {
+            return new EntityValue<TValue>(value);
+        }
+    }
+}
