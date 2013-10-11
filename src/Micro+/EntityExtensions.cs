e88842842da026a41364ca666855148e3e900ac0
PowerMogli <PowerMogli@users.noreply.github.com>
2013-10-11 14:05:16 +0200

    Transactional execution for update and insert statements.

--- src/Micro+/EntityExtensions.cs
+++ src/Micro+/EntityExtensions.cs
@@ -1,26 +1,42 @@
 
+using System.Data;
 namespace MicroORM.Base
 {
     public static class EntityExtensions
     {
         public static void Load<TEntity>(this TEntity entity) where TEntity : Entity, new()
         {
-            IDbSession dbSession = entity.DbSession;
-            entity = dbSession.Load(entity);
-            entity.DbSession = dbSession;
-            entity.EntityState = EntityState.Loaded;
+            using (IDbSession dbSession = entity.DbSession)
+            {
+                entity = dbSession.Load(entity);
+                entity.DbSession = dbSession;
+                entity.EntityState = EntityState.Loaded;
+            }
         }
 
         public static void Update<TEntity>(this TEntity entity) where TEntity : Entity
         {
-            IDbSession dbSession = entity.DbSession;
-            dbSession.Update(entity);
+            using (IDbSession dbSession = entity.DbSession)
+            {
+                using (IDbTransaction transaction = dbSession.BeginTransaction(IsolationLevel.ReadUncommitted))
+                {
+                    dbSession.Update(entity);
+                    transaction.Commit();
+                }
+            }
         }
 
         public static void Insert<TEntity>(this TEntity entity) where TEntity : Entity
         {
-            IDbSession dbSession = entity.DbSession;
-            dbSession.Insert(entity);
+            using (IDbSession dbSession = entity.DbSession)
+            {
+                using (IDbTransaction transaction = dbSession.BeginTransaction(IsolationLevel.ReadUncommitted))
+                {
+                    dbSession.Insert(entity);
+                    transaction.Commit();
+                }
+            }
         }
     }
 }
+
