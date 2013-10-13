a5981164bb6d9d01a953934b6d461a35727e11d2
PowerMogli <albix@gmx.net>
2013-10-13 23:47:36 +0200

    Removed tuple solution.

--- src/Micro+/Storage/SqlDbProvider.cs
+++ src/Micro+/Storage/SqlDbProvider.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Data;
-using MicroORM.Base;
-using MicroORM.Query;
 
 namespace MicroORM.Storage
 {
@@ -39,35 +36,31 @@ namespace MicroORM.Storage
             return "[" + value + "]";
         }
 
-        static ConcurrentDictionary<Type,Tuple<bool,bool,bool>> _meta=new ConcurrentDictionary<Type, Tuple<bool, bool, bool>>();
         public override void SetupParameter(IDbDataParameter parameter, string name, object value)
         {
             base.SetupParameter(parameter, name, value);
             if (value == null) return;
 
+            DealWithSpecialParameterValues(parameter, value);
+        }
+
+        private static void DealWithSpecialParameterValues(IDbDataParameter parameter, object value)
+        {
             Type valueType = value.GetType();
 
-            Tuple<bool, bool, bool> meta = null;
-
-            if (!_meta.TryGetValue(valueType, out meta))
-            {
-                meta = new Tuple<bool, bool, bool>(valueType == typeof(string), valueType.Name == "SqlGeography", valueType.Name == "SqlGeometry");
-                _meta.TryAdd(valueType, meta);
-            }
-
-            if (meta.Item1)
+            if (valueType == typeof(string))
             {
                 parameter.Size = Math.Max(((string)value).Length + 1, 4000);
             }
-            else if (meta.Item2) //SqlGeography is a CLR Type
+            else if (valueType.Name == "SqlGeography")
             {
-                dynamic p = parameter;
-                p.UdtTypeName = "geography";
+                dynamic param = parameter;
+                param.UdtTypeName = "geography";
             }
-            else if (meta.Item3) //SqlGeometry is a CLR Type
+            else if (valueType.Name == "SqlGeometry") //SqlGeography is a CLR Type
             {
-                dynamic p = parameter;
-                p.UdtTypeName = "geometry";
+                dynamic param = parameter;
+                param.UdtTypeName = "geometry";
             }
         }
     }
