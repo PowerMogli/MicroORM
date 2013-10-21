44de8f3c4c375e4c0326e687d7c4903a01ea77f4
PowerMogli <albix@gmx.net>
2013-10-22 01:23:39 +0200

    When entity dies entityInfo dies too.

--- src/Micro+/Entity/Entity.cs
+++ src/Micro+/Entity/Entity.cs
@@ -6,6 +6,7 @@ namespace MicroORM.Entity
     public abstract class Entity
     {
         internal IDbSession EntitySession { get; set; }
+        internal EntityInfo EntityInfo { get; private set; }
         public bool Delete { get; set; }
 
         public Entity()
@@ -13,6 +14,12 @@ namespace MicroORM.Entity
             string connectionString = Registrar<string>.GetFor(this.GetType());
             DbEngine dbEngine = Registrar<DbEngine>.GetFor(this.GetType());
             this.EntitySession = new DbSession(connectionString, dbEngine);
+            this.EntityInfo = new EntityInfo();
+        }
+
+        ~Entity()
+        {
+            this.EntityInfo = null;
         }
     }
 }
