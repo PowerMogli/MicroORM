a96cc17327b71cff62d507e4b9d7daffffe8f8b9
PowerMogli <albix@gmx.net>
2013-10-22 01:22:39 +0200

    Collection for fetched entity values from db to track changes made on the entity.

---
+++ src/Micro+/Entity/EntityValueCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MicroORM.Entity
+{
+    internal class EntityValueCollection : Collection<KeyValuePair<string, object>>
+    {
+    }
+}
