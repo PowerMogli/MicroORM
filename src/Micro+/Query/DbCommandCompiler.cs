d36c1062c37759c76cb5cb5c049dfdd521c989af
PowerMogli <albix@gmx.net>
2013-10-24 00:08:30 +0200

    Renaming of dbcommand compiler.

---
+++ src/Micro+/Query/DbCommandCompiler.cs
@@ -0,0 +1,40 @@
+using System.Data;
+using MicroORM.Storage;
+
+namespace MicroORM.Query
+{
+    internal class DbCommandCompiler
+    {
+        private IArgumentQuery _query;
+        private IDbProvider _provider;
+        private IDbCommand _command;
+
+        internal DbCommandCompiler(IArgumentQuery query, IDbProvider provider)
+        {
+            _query = query;
+            _provider = provider;
+            _command = provider.CreateCommand();
+        }
+
+        internal IDbCommand Compile()
+        {
+            SetupParameter();
+            _command.CommandText = _query.SqlStatement;
+
+            return _command;
+        }
+
+        private void SetupParameter()
+        {
+            if (_query.Arguments == null) return;
+
+            foreach (QueryParameter argument in _query.Arguments)
+            {
+                IDbDataParameter parameter = _command.CreateParameter();
+                parameter.Setup(argument, _provider.ParameterPrefix);
+
+                _command.Parameters.Add(parameter);
+            }
+        }
+    }
+}
