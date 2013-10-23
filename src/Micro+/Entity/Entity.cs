02c25a724991ddd30a5e76804a2ee4ce16ff062d
PowerMogli <albix@gmx.net>
2013-10-24 00:12:16 +0200

    Adjusted for better change tracking.

--- src/Micro+/Entity/Entity.cs
+++ src/Micro+/Entity/Entity.cs
@@ -1,25 +1,12 @@
-using MicroORM.Base;
-using MicroORM.Storage;
-
 namespace MicroORM.Entity
 {
     public abstract class Entity
     {
-        internal IDbSession EntitySession { get; set; }
-        internal EntityInfo EntityInfo { get; private set; }
+        internal EntityState EntityState { get; set; }
         public bool Delete { get; set; }
 
         public Entity()
         {
-            string connectionString = Registrar<string>.GetFor(this.GetType());
-            DbEngine dbEngine = Registrar<DbEngine>.GetFor(this.GetType());
-            this.EntitySession = new DbSession(connectionString, dbEngine);
-            this.EntityInfo = new EntityInfo();
-        }
-
-        ~Entity()
-        {
-            this.EntityInfo = null;
         }
     }
 }
