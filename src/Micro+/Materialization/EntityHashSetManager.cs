eb8b85e54ce3505cf67976802f59afe4ce018537
PowerMogli <albix@gmx.net>
2013-10-24 20:54:48 +0200

    Removed obsolete methods.

--- src/Micro+/Materialization/EntityHashSetManager.cs
+++ src/Micro+/Materialization/EntityHashSetManager.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using MicroORM.Reflection;
-using MicroORM.Entity;
-using System.Linq;
 
 namespace MicroORM.Materialization
 {
@@ -16,22 +14,6 @@ namespace MicroORM.Materialization
                 _entityHashSet.Add(kvp.Key, kvp.Value != null ? kvp.Value.GetHashCode() : -1);
             }
             return _entityHashSet;
-        }
-
-        internal static KeyValuePair<string, object>[] ComputeUpdateValues<TEntity>(TEntity entity, EntityInfo entityInfo)
-        {
-            Dictionary<string, int> entityHashSet = ComputeEntityHashSet(entity);
-            KeyValuePair<string, object>[] entityValues = ParameterTypeDescriptor.ToKeyValuePairs(new object[] { entity });
-
-            Dictionary<string, object> valuesToUpdate = new Dictionary<string, object>();
-            foreach (var kvp in entityHashSet)
-            {
-                var oldHash = entityInfo.EntityHashSet[kvp.Key];
-                if (oldHash.Equals(kvp.Value) == false)
-                    valuesToUpdate.Add(kvp.Key, entityValues.FirstOrDefault(kvp1 => kvp1.Key == kvp.Key).Value);
-            }
-
-            return valuesToUpdate.ToArray();
         }
     }
 }
