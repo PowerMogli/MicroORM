1693cb677ca8f1beb22d9abecc25fe5d9a2004d4
PowerMogli <albix@gmx.net>
2013-10-24 18:37:39 +0200

    Changed hash value to string.

--- src/Micro+/Entity/EntityInfo.cs
+++ src/Micro+/Entity/EntityInfo.cs
@@ -8,13 +8,13 @@ namespace MicroORM.Entity
         internal EntityInfo()
         {
             this.EntityState = EntityState.None;
-            this.EntityHashSet = new Dictionary<string, int>();
+            this.EntityHashSet = new Dictionary<string, string>();
             this.LastCallTime = DateTime.Now;
         }
 
         internal bool CanBeRemoved { get { return DateTime.Now.Subtract(this.LastCallTime) > TimeSpan.FromMinutes(2); } }
         internal EntityState EntityState { get; set; }
-        internal Dictionary<string, int> EntityHashSet { get; set; }
+        internal Dictionary<string, string> EntityHashSet { get; set; }
         internal DateTime LastCallTime { get; set; }
     }
 }
