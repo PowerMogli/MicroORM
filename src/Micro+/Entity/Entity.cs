d85d10cd400bfe4c820293552ebada2f4d7b746d
PowerMogli <albix@gmx.net>
2013-10-24 20:51:39 +0200

    Removed unused and obsolete properties.

--- src/Micro+/Entity/Entity.cs
+++ src/Micro+/Entity/Entity.cs
@@ -2,11 +2,6 @@ namespace MicroORM.Entity
 {
     public abstract class Entity
     {
-        internal EntityState EntityState { get; set; }
-        public bool Delete { get; set; }
-
-        public Entity()
-        {
-        }
+        public Entity() { }
     }
 }
