b41d7b77624b7cc81ba6fdd91eca2a4db6502f20
PowerMogli <albix@gmx.net>
2013-10-27 20:03:22 +0100

    Load alternative with costum mapping.

--- src/RabbitDB/Entity/EntityExtensions.cs
+++ src/RabbitDB/Entity/EntityExtensions.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Data;
 using RabbitDB.Base;
 using RabbitDB.Caching;
+using RabbitDB.Query;
 using RabbitDB.Storage;
 
 namespace RabbitDB.Entity
@@ -12,30 +14,59 @@ namespace RabbitDB.Entity
             EntityInfo entityInfo = EntityInfoCacheManager.GetEntityInfo(entity);
             if (entityInfo.EntityState == EntityState.Loaded) return;
 
-            string connectionString = Registrar<string>.GetFor(entity.GetType());
-            DbEngine dbEngine = Registrar<DbEngine>.GetFor(entity.GetType());
+            Tuple<string, DbEngine> result = InitializeSession<TEntity>();
 
-            using (IDbSession entitySession = new DbSession(connectionString, dbEngine))
+            using (IDbSession entitySession = new DbSession(result.Item1, result.Item2))
             {
                 entitySession.Load(entity);
+
                 entityInfo.EntityState = EntityState.Loaded;
                 entityInfo.ComputeSnapshot(entity);
             }
         }
 
-        public static bool PersistChanges<TEntity>(this TEntity entity, bool delete = false) where TEntity : Entity
+        public static void Load<TEntity>(this TEntity entity, Action<TEntity, IDataReader> customMapper) where TEntity : Entity, new()
         {
-            string connectionString = Registrar<string>.GetFor(entity.GetType());
-            DbEngine dbEngine = Registrar<DbEngine>.GetFor(entity.GetType());
+            EntityInfo entityInfo = EntityInfoCacheManager.GetEntityInfo(entity);
+            if (entityInfo.EntityState == EntityState.Loaded) return;
+
+            Tuple<string, DbEngine> result = InitializeSession<TEntity>();
+
+            using (IDbSession entitySession = new DbSession(result.Item1, result.Item2))
+            {
+                EntityReader<TEntity> entityReader = entitySession.GetEntityReader<TEntity>(new EntityQuery<TEntity>(entity));
+                entityReader.Load(entity, customMapper);
+
+                entityInfo.EntityState = EntityState.Loaded;
+                entityInfo.ComputeSnapshot(entity);
+            }
+        }
+
+        internal static Tuple<string, DbEngine> InitializeSession<TEntity>()
+        {
+            string connectionString = Registrar<string>.GetFor(typeof(TEntity));
+            DbEngine dbEngine = Registrar<DbEngine>.GetFor(typeof(TEntity));
+
+            return new Tuple<string, DbEngine>(connectionString, dbEngine);
+        }
+
+        public static void MarkForDeletion<TEntity>(this TEntity entity) where TEntity : Entity
+        {
+            entity.MarkedForDeletion = true;
+        }
+
+        public static bool PersistChanges<TEntity>(this TEntity entity) where TEntity : Entity
+        {
+            Tuple<string, DbEngine> result = InitializeSession<TEntity>();
             EntityInfo entityInfo = EntityInfoCacheManager.GetEntityInfo(entity);
 
-            using (IDbSession dbSession = new DbSession(connectionString, dbEngine))
+            using (IDbSession dbSession = new DbSession(result.Item1, result.Item2))
             {
                 try
                 {
                     using (IDbTransaction transaction = dbSession.BeginTransaction(IsolationLevel.ReadUncommitted))
                     {
-                        if (dbSession.PersistChanges(entity, delete) == false) return false;
+                        if (dbSession.PersistChanges(entity) == false) return false;
 
                         transaction.Commit();
                         entityInfo.MergeChanges();
