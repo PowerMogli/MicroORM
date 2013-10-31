be881f4c1fb5c218546b65420d7812f4e6e4fc01
PowerMogli <PowerMogli@users.noreply.github.com>
2013-10-31 15:40:02 +0100

    Create UpdateQuery.cs

---
+++ src/RabbitDB/Query/Generic/UpdateQuery.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
+using RabbitDB.Expressions;
+using RabbitDB.Reflection;
+using RabbitDB.Storage;
+
+namespace RabbitDB.Query.Generic
+{
+    internal class UpdateQuery<TEntity> : IQuery
+    {
+        private readonly Expression<Func<TEntity, bool>> _expression;
+        private readonly object[] _arguments;
+
+        internal UpdateQuery(Expression<Func<TEntity, bool>> condition, params object[] arguments)
+        {
+            _expression = condition;
+            _arguments = arguments;
+        }
+
+        public IDbCommand Compile(IDbProvider dbProvider)
+        {
+            UpdateTableBuilder<TEntity> updateTableBuilder = new UpdateTableBuilder<TEntity>(dbProvider);
+            foreach (KeyValuePair<string, object> parameter in ParameterTypeDescriptor.ToKeyValuePairs(_arguments))
+            {
+                updateTableBuilder.Set(parameter.Key, parameter.Value);
+            }
+            updateTableBuilder.Where(_expression);
+
+            SqlQuery sqlQuery = new SqlQuery(updateTableBuilder.GetSql(), QueryParameterCollection.Create<TEntity>(updateTableBuilder.GetParameters()));
+            return sqlQuery.Compile(dbProvider);
+        }
+    }
+}
