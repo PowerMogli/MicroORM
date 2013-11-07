886918aab1d27cea2f984aa55b1cb824d29c227b
PowerMogli <albix@gmx.net>
2013-11-07 16:58:47 +0100

    Method to prepare statement and parameters.

--- src/RabbitDB/Expression/UpdateTableBuilder.cs
+++ src/RabbitDB/Expression/UpdateTableBuilder.cs
@@ -10,8 +10,10 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using RabbitDB.Mapping;
+using RabbitDB.Query;
 using RabbitDB.Storage;
 
 namespace RabbitDB.Expressions
@@ -62,6 +64,16 @@ namespace RabbitDB.Expressions
         {
             _builder.EndEnumeration();
             _builder.Where(criteria);
+        }
+
+        internal static Tuple<string, QueryParameterCollection> PrepareForUpdate<TEntity>(TEntity entity, IDbProvider dbProvider, KeyValuePair<string, object>[] valuesToUpdate)
+        {
+            TableInfo tableInfo = TableInfo<TEntity>.GetTableInfo;
+            string updateStatement = tableInfo.CreateUpdateStatement(dbProvider, valuesToUpdate);
+            QueryParameterCollection queryParameterCollection = QueryParameterCollection.Create<TEntity>(new object[] { valuesToUpdate });
+            queryParameterCollection.AddRange(tableInfo.GetPrimaryKeyValues<TEntity>(entity));
+
+            return new Tuple<string, QueryParameterCollection>(updateStatement, queryParameterCollection);
         }
 
         public string GetSql()
